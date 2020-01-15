61b1bbb54ddf819651ccccce3185d16d8160ec36
hitaichou <holyfernandes@yahoo.co.uk>
2020-01-14 21:06:33 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploForEach/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExemploForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
